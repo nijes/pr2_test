92964816eea6dcc2ee376c74ee32dc728fb5d968
Ubuntu <ubuntu@ip-172-31-28-56.ap-northeast-1.compute.internal>
2022-08-16 16:03:45 +0900

    nginx update

--- predict_gas/settings.py
+++ predict_gas/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'predict_gas',
     'bootstrap4'
-    'restdjango'
 ]
 
 MIDDLEWARE = [
@@ -129,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
